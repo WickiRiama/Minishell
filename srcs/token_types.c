83dccf63f500d719b8308edc0446801f637c309c
Margaux Riant <mriant@student.42.fr>
2022-06-28 18:24:04 +0200

    Add functions to caracterize redirections.
    Still need a function to deal with syntax errors.
    WIP, need to save work.
    
     On branch token_types
     Changes to be committed:
    	modified:   srcs/token_types.c
    	modified:   srcs/tokenisation.c

--- srcs/token_types.c
+++ srcs/token_types.c
@@ -6,7 +6,7 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 09:22:11 by mriant            #+#    #+#             */
-/*   Updated: 2022/06/28 15:25:39 by mriant           ###   ########.fr       */
+/*   Updated: 2022/06/28 18:23:26 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,12 +44,50 @@ int	ft_is_blank(char c)
 	return (0);
 }
 
+void	ft_redirin_type(t_token *tokens)
+{
+	if (ft_strcmp(tokens->token, "<") == 0)
+	{
+		ft_strlcpy(tokens->type, "redir_from", ft_strlen("redir_from") + 1);
+		if (tokens->next)
+			ft_strlcpy(tokens->next->type, "infile", ft_strlen("infile") + 1);
+	}
+	else if (ft_strcmp(tokens->token, "<<") == 0)
+	{
+		ft_strlcpy(tokens->type, "here_doc", ft_strlen("here_doc") + 1);
+		if (tokens->next)
+			ft_strlcpy(tokens->next->type, "delim", ft_strlen("delim") + 1);
+	}
+	if (!tokens->next)
+		ft_error("Syntax error");
+}
+
+void	ft_redirout_type(t_token *tokens)
+{
+	else if (ft_strcmp(tokens->token, ">") == 0)
+	{
+		ft_strlcpy(tokens->type, "redir_to", ft_strlen("redir_to") + 1);
+		if (tokens->next)
+			ft_strlcpy(tokens->next->type, "outfile", ft_strlen("outfile") + 1);
+	}
+	else if (ft_strcmp(tokens->token, ">>") == 0)
+	{
+		ft_strlcpy(tokens->type, "append", ft_strlen("append") + 1);
+		if (tokens->next)
+			ft_strlcpy(tokens->next->type, "app_file", ft_strlen("app_file") + 1);
+	}
+	if (!tokens->next)
+		ft_error("Syntax error");
+}
+
 void	ft_token_types(t_token *tokens)
 {
 	while (tokens)
 	{
-		if (ft_strcmp(tokens->type, "operator") == 0)
-			ft_op_type();
+		if (tokens->token[0] == '<')
+			ft_redirin_type(tokens);
+		if (tokens->token[0] == '>')
+			ft_redirout_type(tokens);
 		else
 			ft_word_type();
 		tokens = tokens->next;
