00bb33d1ec436a8c95f7ecc7a1f1624ba0225718
Samantha Le Huec <s.lhuec@gmail.com>
2022-07-06 13:26:15 +0200

    correctif norminette ECHO: espace a la fin corrige  On branch builtin  Your branch is up to date with 'origin/builtin'.
     Changes to be committed:
    	modified:   srcs/echo.c
    	modified:   srcs/exit.c

--- srcs/echo.c
+++ srcs/echo.c
@@ -6,7 +6,7 @@
 /*   By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 15:48:14 by sle-huec          #+#    #+#             */
-/*   Updated: 2022/07/06 12:16:01 by sle-huec         ###   ########.fr       */
+/*   Updated: 2022/07/06 13:22:18 by sle-huec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	ft_echo(char **input)
 	while (input[i])
 	{
 		ft_printf("%s", input[i]);
-		if (i + 1)
+		if (input[i + 1])
 			ft_printf("%c", ' ');
 		i++;
 	}
