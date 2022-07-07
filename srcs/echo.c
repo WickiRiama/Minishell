bfaa96e7373329b37b57c4bd632979a34d775d01
Samantha Le Huec <s.lhuec@gmail.com>
2022-07-07 16:39:52 +0200

    modifs echo  Changes to be committed: 	modified:   srcs/cd.c 	modified:   srcs/echo.c

--- srcs/echo.c
+++ srcs/echo.c
@@ -6,7 +6,7 @@
 /*   By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 15:48:14 by sle-huec          #+#    #+#             */
-/*   Updated: 2022/07/06 13:22:18 by sle-huec         ###   ########.fr       */
+/*   Updated: 2022/07/07 16:24:25 by sle-huec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,13 @@ int	ft_echo(char **input)
 
 	nl = 0;
 	i = 1;
+
 	if (ft_strcmp(input[1], "-n") == 0)
 	{
 		nl = 1;
 		i++;
 	}
+
 	while (input[i])
 	{
 		ft_printf("%s", input[i]);
