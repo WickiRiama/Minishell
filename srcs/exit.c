65cf029a353a92056291b76840f9b8d4cfbd52cb
Samantha Le Huec <s.lhuec@gmail.com>
2022-07-07 15:25:54 +0200

    EXIT : Leaks et norminette corrected  On branch builtin  Your branch is up to date with 'origin/builtin'.
     Changes to be committed:
    modified:   srcs/exit.c

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,7 +6,7 @@
 /*   By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 13:25:07 by sle-huec          #+#    #+#             */
-/*   Updated: 2022/07/07 14:35:38 by sle-huec         ###   ########.fr       */
+/*   Updated: 2022/07/07 15:20:48 by sle-huec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	len_arg_tab(char **input)
 	int	i;
 
 	i = 0;
-	while(input && input[i])
+	while (input && input[i])
 	{
 		i++;
 	}
@@ -34,7 +34,7 @@ int	check_arg_and_get_status(char **input)
 	int	len;
 
 	i = 0;
-	while(input[1])
+	while (input[1])
 	{
 		if (!ft_isdigit(input[1][i]))
 		{
@@ -59,12 +59,12 @@ int	check_arg_and_get_status(char **input)
 
 int	ft_exit(t_token **tokens, char **input, char **env)
 {
-	int status;
-	
+	int	status;
+
 	ft_printf("exit\n");
 	status = check_arg_and_get_status(input);
 	ft_lstclear_msh(tokens, &free);
-	free(input);
-	free_tab(env);	
+	free_tab(input);
+	free_tab(env);
 	exit(status);
 }
