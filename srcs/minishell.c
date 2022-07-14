397dc7b06e4a1779767f9997bba6319015f79549
Samantha Le Huec <sle-huec@student.42.fr>
2022-07-14 18:19:49 +0200

    pull from master
     Changes to be committed:
    	modified:   srcs/minishell.c

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -42,26 +42,6 @@ void	ft_print_list(t_dlist *list, t_dlist *pipes)
 	}
 }
 
-<<<<<<< HEAD
-=======
-void	found_and_run_cmd(char **cmd, t_env *env, t_dlist **blocks,
-t_dlist **pipes)
-{
-	if (ft_strcmp(cmd[0], "cd") == 0)
-		ft_cd(cmd, &env);
-	else if (ft_strcmp(cmd[0], "echo") == 0)
-		ft_echo(cmd);
-	if (!env)
-		display_env(env);
-	else if (ft_strcmp(cmd[0], "unset") == 0)
-		ft_unset(cmd, &env);
-	// else if (ft_strcmp(cmd[0], "export") == 0)
-	// 	ft_export(cmd, &env);
-	else if (ft_strcmp(cmd[0], "exit") == 0)
-		ft_exit(cmd, blocks, pipes, &env);
-}
-
->>>>>>> cd : MAJ OLDPWD et PWD dans env
 int	main(int ac, char **av, char **envp)
 {
 	t_env	*env;
@@ -86,38 +66,6 @@ int	main(int ac, char **av, char **envp)
 			return (1);
 		}
 		ft_executor(blocks, pipes, env);
-		ft_lstclear_msh(&blocks, &ft_del_blocks);
-		ft_lstclear_msh(&pipes, &ft_del_pipes);
-	}
-	ft_lstclear_env(&env, &free);
-	return (0);
-}
-
-/*
-int	main(int ac, char **av, char **envp)
-{
-	t_dlist	*blocks;
-	t_dlist	*pipes;
-	t_env	*env;
-
-	if (ac != 1)
-		return (1);
-	(void) av;
-	get_env_var(envp, &env);
-	if (!env)
-		return (1);
-	while (1)
-	{
-		pipes = NULL;
-		blocks = ft_parsing(&pipes, env);
-		if (!blocks)
-		{
-			ft_lstclear_msh(&pipes, &ft_del_pipes);
-			ft_lstclear_env(&env, &free);
-			return (1);
-		}
-		// found_and_run_cmd(&tokens, input, env);
-		ft_print_list(blocks, pipes);
 		ft_lstclear_msh(&blocks, &ft_del_blocks);
 		ft_lstclear_msh(&pipes, &ft_del_pipes);
 	}
