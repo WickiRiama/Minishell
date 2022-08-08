1d4913f5d5e59743a57f13c6e60768ea606ac03b
Margaux Riant <mriant@student.42.fr>
2022-08-08 16:25:58 +0200

    Close a redirection fd when an other one takes its place.
    Check if the value of fd is >= 0 before closing it.
    
     On branch fix_close_old_redir
     Changes to be committed:
    	modified:   Makefile
    	modified:   includes/minishell.h
    	modified:   srcs/exec/exec_clean.c
    	modified:   srcs/parsing/blocks_list.c
    	modified:   srcs/parsing/pipes_list.c
    	new file:   srcs/parsing/redir.c

---
+++ srcs/parsing/redir.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redir.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/08 14:19:18 by mriant            #+#    #+#             */
+/*   Updated: 2022/08/08 16:20:43 by mriant           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+#include "minishell.h"
+#include "libft.h"
+
+void	ft_close_old_redir(t_dlist *tokens, t_exec *blocks)
+{
+	if (((t_token *)tokens->cont)->type == INFILE && blocks->infile >= 0)
+		blocks->infile = close(blocks->infile) - 2;
+	else if (((t_token *)tokens->cont)->type == OUTFILE && blocks->outfile >= 0)
+		blocks->outfile = close(blocks->outfile) - 2;
+	else if (((t_token *)tokens->cont)->type == APP_FILE
+		&& blocks->outfile >= 0)
+		blocks->outfile = close(blocks->outfile) - 2;
+	else if (((t_token *)tokens->cont)->type == DELIM && blocks->infile >= 0)
+		blocks->infile = close(blocks->infile) - 2;
+}
+
+void	ft_open_redir(t_dlist *tokens, t_exec *blocks, t_env **env)
+{
+	if (((t_token *)tokens->cont)->type == INFILE)
+		blocks->infile = open(((t_token *)tokens->cont)->text, O_RDONLY);
+	else if (((t_token *)tokens->cont)->type == OUTFILE)
+		blocks->outfile = open(((t_token *)tokens->cont)->text, \
+			O_WRONLY | O_CREAT | O_TRUNC, 00644);
+	else if (((t_token *)tokens->cont)->type == APP_FILE)
+		blocks->outfile = open(((t_token *)tokens->cont)->text, \
+			O_WRONLY | O_CREAT | O_APPEND, 00644);
+	else if (((t_token *)tokens->cont)->type == DELIM)
+		blocks->infile = ft_here_doc(tokens, env);
+	if (blocks->infile == -1 || blocks->outfile == -1)
+		ft_fprintf(2, "%s: %s\n", \
+			strerror(errno), ((t_token *)tokens->cont)->text);
+}
