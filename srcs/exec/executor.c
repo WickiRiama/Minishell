b2c04a50693ff5eb954dcaa85f3142516fcea043
Margaux Riant <mriant@student.42.fr>
2022-08-08 11:19:07 +0200

    Fix a segfault when there was only a redirection without a command
     On branch fix_single_redir
     Changes to be committed:
    	modified:   srcs/exec/exec_builtin.c
    	modified:   srcs/exec/exec_utils.c
    	modified:   srcs/exec/executor.c

--- srcs/exec/executor.c
+++ srcs/exec/executor.c
@@ -6,7 +6,7 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 15:08:27 by sle-huec          #+#    #+#             */
-/*   Updated: 2022/08/01 16:13:46 by mriant           ###   ########.fr       */
+/*   Updated: 2022/08/08 11:13:21 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,6 +90,8 @@ int	ft_exec(t_dlist	*blocks, t_dlist *pipes, t_env *env)
 	pid_t	pid;
 	char	**env_tab;
 
+	if (!((t_exec *)blocks->cont)->cmd)
+		return (-2);
 	env_tab = ft_list_to_tab(env);
 	if (!env_tab)
 		return (-1);
