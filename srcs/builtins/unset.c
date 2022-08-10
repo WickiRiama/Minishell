d99731b8da940a5839cf9b7fd3e360d6f1c176f3
Samantha Le Huec <sle-huec@student.42.fr>
2022-08-10 09:37:17 +0200

    script de test exit ajout commandes test unset  On branch builtin2 	modified:   parsing_test_cases.txt 	modified:   srcs/builtins/unset.c 	new file:   test_exit.bash

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -13,98 +13,51 @@
 #include "minishell.h"
 #include "libft.h"
 
-// int	is_syntax_err(char *input))
-// {
-
-// }
-
-// int	is_invalid_option(char *input)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	if ((input[i] == '-') && (input[i + 1]))
-// 		return (1);
-// 	else
-// 		return (0);
-// }
-
-// int	get_return_value(char *input, t_env **env)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	if (ft_get_ptr_env_var(input, env) != NULL)
-// 	{
-
-// 	}
-// 	else if (is_invalid_option(input))
-// 		return (2);
-// 	while (input[i])
-// 	{
-// 		// if (is_syntax_err(input[i]))
-// 		// 	return (2);
-// 		if (!ft_isalpha(input[i]))
-// 			return (1);
-// 		i++;
-// 	}
-// 	return (0);
-// }
-
-// void	print_error(int ret)
-// {
-// 	if (ret == 1)
-
-// 	else if ()
-// }
-
-// int	ft_unset(char **input, t_env **env)
-// {
-// 	int	i;
-// 	int	ret;
-
-// 	i = 1;
-// 	while (input[i])
-// 	{
-// 		ret = get_ret(input, env);
-// 		if (ret == 0)
-// 			return (ret);
-// 		else
-// 		{
-// 			print_error(ret);
-// 			return (ret);
-// 		}
-// 		i++;
-// 	}
-// 	return (0);
-// }
-
-int	check_arg(char **input)
+int	is_invalid_option(char *input)
 {
-	if (input[1])
-	{
-		if (check_digit(input))
-			return (2);
-		if (len > 2)
-			return (-1);
-		else
-			return (ft_atoi(input[1]));
-	}
+	if ((input[0] == '-') && (input[1]))
+		return (1);
 	else
-		return (g_exitcode);
+		return (0);
+}
+
+int	get_return_value(char *input)
+{
+	int	i;
+
+	i = 0;
+
+	if (is_invalid_option(input))
+		return (2);
+	else
+	{
+		while (input[i])
+		{
+			if (!ft_isalpha(input[i]))
+				return (1);
+			i++;
+		}
+	}
+	return (0);
 }
 
 int	ft_unset(char **input, t_env **env)
 {
 	int	i;
+	int	ret;
 
 	i = 1;
-
+	ret = 0;
 	while ((input[i]) && (i <= len_arg_tab(input)))
 	{
 		if (ft_get_ptr_env_var(input[i], *env) != NULL)
+		{
 			ft_lstdelone_env(ft_get_ptr_env_var(input[i], *env), &free);
+			ret = 0;
+		}
+		else
+			ret = get_return_value(input[i]);
 		i++;
 	}
-	return (0);
+	return (ret);
 }
