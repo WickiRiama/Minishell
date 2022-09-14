df5aaf7c227a229c5dc4c299b08ed21fa4f35598
Margaux Riant <mriant@student.42.fr>
2022-09-14 10:04:18 +0200

    Placed the here_doc file in /temp/ repositiry instead of current repository
     On branch fix_here_doc
     Changes to be committed:
    	modified:   srcs/parsing/here_doc/here_doc.c

--- srcs/parsing/here_doc/here_doc.c
+++ srcs/parsing/here_doc/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 17:24:59 by mriant            #+#    #+#             */
-/*   Updated: 2022/09/12 18:08:02 by mriant           ###   ########.fr       */
+/*   Updated: 2022/09/14 10:02:07 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@
 #include "minishell.h"
 #include "libft.h"
 
-void	ft_find_here_name(char *s1, char name[11])
+void	ft_find_here_name(char *s1, char name[17])
 {
 	int		i;
 
 	i = 0;
-	while (i < 10 && s1[i])
+	while (i < 16 && s1[i])
 	{
 		name[i] = s1[i];
 		i++;
@@ -37,8 +37,8 @@ void	ft_find_here_name(char *s1, char name[11])
 	i = 1;
 	while (i < 100 && access(name, F_OK) == 0)
 	{
-		name[8] = '0' + i / 10;
-		name[9] = '0' + i % 10;
+		name[15] = '0' + i / 10;
+		name[16] = '0' + i % 10;
 		i++;
 	}
 }
@@ -73,7 +73,7 @@ void	ft_init_here_space(int *temp_std, int *temp_exit, t_sig *new_sa)
 	ft_set_sa(new_sa, &ft_handle_here_doc);
 }
 
-void	ft_close_here_space(int *temp_std, int *fd, char name[11],
+void	ft_close_here_space(int *temp_std, int *fd, char name[17],
 	int *temp_exit)
 {
 	dup2(*temp_std, 0);
@@ -90,13 +90,13 @@ void	ft_close_here_space(int *temp_std, int *fd, char name[11],
 
 int	ft_here_doc(t_dlist *tokens, t_env **env, t_sig *new_sa)
 {
-	char	name[11];
+	char	name[17];
 	int		fd;
 	int		temp_std;
 	int		temp_exit;
 
 	ft_init_here_space(&temp_std, &temp_exit, new_sa);
-	ft_find_here_name("here_doc00", name);
+	ft_find_here_name("/tmp/.here_doc00", name);
 	fd = open(name, O_WRONLY | O_CREAT, 00644);
 	if (fd == -1)
 		return (fd);
